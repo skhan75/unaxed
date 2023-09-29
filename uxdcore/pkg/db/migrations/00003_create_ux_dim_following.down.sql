d6dcecd9b08c6c1ef5b71aeec7ec8d513453c7fe
skhan75 <sami.ahmadkhan12@gmail.com>
2023-09-28 22:58:30 -0700

    Renaming backend name to uxserver

--- uxdcore/pkg/db/migrations/00003_create_ux_dim_following.down.sql
+++
@@ -1,2 +0,0 @@
--- +migrate Down
-DROP TABLE IF EXISTS ux_dim_following;
--- uxdcore/pkg/db/migrations/00003_create_ux_dim_following.up.sql
+++
@@ -1,9 +0,0 @@
--- +migrate Up
-CREATE TABLE ux_dim_following (
-    following_id BIGINT NOT NULL,
-    user_id BIGINT NOT NULL,
-    started_following_on TIMESTAMP NOT NULL,
-    FOREIGN KEY (following_id) REFERENCES ux_dim_users(id),
-    FOREIGN KEY (user_id) REFERENCES ux_dim_users(id)
-);
-        
